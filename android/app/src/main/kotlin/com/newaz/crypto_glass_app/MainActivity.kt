0d8bcf2390150a0d8c8bc1e412009cedc13e7cda
Saif Rahman <saif.rahman@udplatforms.com>
2023-08-05 19:56:03 +0600

    glass

---
+++ android/app/src/main/kotlin/com/newaz/crypto_glass_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.newaz.crypto_glass_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+
+class SearchNav extends StatelessWidget {
+  const SearchNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Row(
+        children: [
+          const Expanded(
+            child: Text(
+              'Explore Collections',
+              style: TextStyle(fontSize: 52, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 35, horizontal: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              border: Border.all(
+                width: 5,
+                color: const Color(0xFFE8F0EE),
+              ),
+            ),
+            child: const Icon(CupertinoIcons.search),
+          ),
+        ],
+      ),
+    );
+  }
+}
